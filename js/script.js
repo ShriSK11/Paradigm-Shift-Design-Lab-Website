1e1dc96b3d2caa98486dc84f97d4ddb9bf364900
ShriSK11 <73185687+ShriSK11@users.noreply.github.com>
2022-06-03 04:11:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
-$(document).ready(function() {
-    $('#myContainer').multiscroll({
-        anchors: ['first', 'second', 'third'],
-        menu: '#menu',
-        css3: true,
-        
-        //equivalent to jQuery `easeOutBack` extracted from http://matthewlein.com/ceaser/
-        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
-    });
-});
-
-
-
-
-
 (function ($) {
     "use strict";
 
 
-    
+
+
+
+    $(window).on("load", function() {
+        $("#preloader").fadeOut(1200);
+		$("#preloader-status").delay(200).fadeOut("slow");
+        // changeHeight();
+        // $(".footer-opener").addClass("active");
+        resetfooter();
+    });
+
+    setTimeout(function() {
+        $(".preloader-bg").addClass("preloader-bg-disappear");
+        $(".ms-left, .ms-right").addClass("fadeIn");
+    }, 1000);
+
+    $(document).ready(function () {
+        $('#myContainer').multiscroll({
+            anchors: ['first', 'second', 'third'],
+            menu: '#menu',
+            css3: true,
+
+            //equivalent to jQuery `easeOutBack` extracted from http://matthewlein.com/ceaser/
+            easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
+        });
 
         $("#left-bg-1").vegas({
             transition: 'slideUp',
@@ -164,102 +173,108 @@ $(document).ready(function() {
         });
 
 
-       
+        $("#section-bg-1").vegas({
+            transition: 'slideUp',
+            // transitionDuration: 4000,
+            timer: false,
+            delay: 5000,
+            cover: true,
+            align: 'center',
+            valign: 'right',
+            // animation: 'kenburns',
+            // animationDuration: 20000,
+            loop: true,
+            slides: [{
+                    src: 'images/1-sec1.jpg'
+                },
+                {
+                    src: 'images/1-sec2.jpg'
+                },
+                {
+                    src: 'images/1-sec3.jpg'
+                }
 
+            ]
+        });
+
+
+
+
+
+        $("#section-bg-2").vegas({
+            transition: 'slideUp',
+            // transitionDuration: 4000,
+            timer: false,
+            delay: 5000,
+            cover: true,
+            align: 'center',
+            valign: 'right',
+            // animation: 'random',
+            // animationDuration: 20000,
+            loop: true,
+            slides: [{
+                    src: 'images/2-sec1.jpg'
+                },
+                {
+                    src: 'images/2-sec2.jpg'
+                },
+                {
+                    src: 'images/2-sec3.jpg'
+                }
+
+            ]
+        });
+
+        $("#section-bg-3").vegas({
+            transition: 'slideUp',
+            // transitionDuration: 4000,
+            timer: false,
+            delay: 5000,
+            cover: true,
+            align: 'center',
+            valign: 'right',
+            // animation: 'random',
+            // animationDuration: 20000,
+            loop: true,
+            slides: [{
+                    src: 'images/4-left1.png'
+                },
+                {
+                    src: 'images/4-left2.png'
+                },
+                {
+                    src: 'images/4-left3.png'
+                }
+
+            ]
+        });
+
+        window.onload = changeHeight();
+        const hamburger = document.querySelector(".hamburger");
+        const navLinks = document.querySelector(".nav-links");
+        const links = document.querySelectorAll(".nav-links li");
+
+        hamburger.addEventListener('click', () => {
+            //Animate Links
+            navLinks.classList.toggle("open");
+            links.forEach(link => {
+                link.classList.toggle("fade");
+            });
+
+            //Hamburger Animation
+            hamburger.classList.toggle("toggle");
+        });
+
+
+
+
+
+    });
+    
 })(jQuery);
 
-
-
-
-$("#section-bg-1").vegas({
-    transition: 'slideUp',
-    // transitionDuration: 4000,
-    timer: false,
-    delay: 5000,
-    cover: true,
-    align: 'center',
-    valign: 'right',
-    // animation: 'kenburns',
-    // animationDuration: 20000,
-    loop: true,
-    slides: [{
-            src: 'images/1-sec1.jpg'
-        },
-        {
-            src: 'images/1-left2.png'
-        },
-        {
-            src: 'images/1-left3.png'
-        }
-
-    ]
-});
-
-$("#section-bg-2").vegas({
-    transition: 'slideUp',
-    // transitionDuration: 4000,
-    timer: false,
-    delay: 5000,
-    cover: true,
-    align: 'center',
-    valign: 'right',
-    // animation: 'random',
-    // animationDuration: 20000,
-    loop: true,
-    slides: [{
-            src: 'images/2-left1.png'
-        },
-        {
-            src: 'images/2-left2.png'
-        },
-        {
-            src: 'images/2-left3.png'
-        }
-
-    ]
-});
-
-$("#section-bg-3").vegas({
-    transition: 'slideUp',
-    // transitionDuration: 4000,
-    timer: false,
-    delay: 5000,
-    cover: true,
-    align: 'center',
-    valign: 'right',
-    // animation: 'random',
-    // animationDuration: 20000,
-    loop: true,
-    slides: [{
-            src: 'images/4-left1.png'
-        },
-        {
-            src: 'images/4-left2.png'
-        },
-        {
-            src: 'images/4-left3.png'
-        }
-
-    ]
-});
-
-
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
-
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
+jQuery(function($){
+    $('.footer-opener').on('click', function(){
+        $(this).toggleClass('active');
     });
-
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-});
-
-
-
-
-
+})
